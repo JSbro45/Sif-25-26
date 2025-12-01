87d066ccfc3f0fa5f73ca705136062a79640f923
JSbro45 <j.samuel.brou@gmail.com>
2025-12-01 06:16:17 +0100

    quick fix

--- next-frame/app/add_event/page.tsx
+++ next-frame/app/add_event/page.tsx
@@ -1,5 +1,5 @@
-import { FormInputBox } from "../../auth/components/FormInput"
-import MapModule from "../../map/components/MapModule"
+import { FormInputBox } from "../auth/components/FormInput"
+import MapModule from "../map/components/MapModule"
 
 
 export default function AddEventForm() {
