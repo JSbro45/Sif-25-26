3e49155c4817b4cb53ff63fc18516eb6559986c4
katdoormat <imsooweirdhaha@gmail.com>
2025-12-04 21:05:52 +0100

    kosmeticky update

--- project-front/src/components/map/PlusBar-PrihlasenyUser.tsx
+++ project-front/src/components/map/PlusBar-PrihlasenyUser.tsx
@@ -3,11 +3,11 @@ import '../../javascript/plusBar.js/*'
 export default function PlusBarPU() {
     return (
         <div className="dropdown">
-        <button className="round-btn" id="ddToggle" aria-expanded="false" aria-controls="ddMenu" title="Open menu" style={{fontSize: "xx-large", color:"#d4ecff"}}>
+        <button className="round-btn" id="ddToggle" title="Open menu" style={{fontSize: "xx-large", color:"#d4ecff"}}>
             +
         </button>
 
-        <div className="menu" id="ddMenu" role="menu" aria-labelledby="ddToggle">
+        <div className="menu" id="ddMenu" role="menu">
             <a href="add_pin_form.html" role="menuitem">Přidat</a>
             <a href="user_account_info.html" role="menuitem">Váš účet</a>
             <a href="project_info_o_nas.html" role="menuitem">O nás</a>
