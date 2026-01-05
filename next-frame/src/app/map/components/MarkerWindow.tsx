81258c3b0be753863d065fcf52b726c93e2ab79b
JSbro45 <j.samuel.brou@gmail.com>
2026-01-05 09:37:48 +0100

    popup restore

--- next-frame/src/app/map/components/MarkerWindow.tsx
+++ next-frame/src/app/map/components/MarkerWindow.tsx
@@ -1,6 +1,5 @@
 import { Icon } from "leaflet";
 import { Marker, Popup } from "react-leaflet";
-import EventView from "./EventView";
 import '../../styles/map.css'
 import '../../styles/popup.css'
 
@@ -14,7 +13,16 @@ export default function MarkerWindow({pos,evt, time}: {pos: [number,number], evt
         <div className="marker" onDoubleClick={(ev) => {}}>
             <Marker position={pos} icon={icon}>
                 <Popup>
-                    <EventView></EventView>
+                    <ul className="event-thumbnail">
+                        <li>
+                            <span className="event"> <b className="n"> event: </b> { evt } </span>
+                        </li>
+                        <li>
+                            <span className="time"> <b className="n"> time: </b>
+                                <time dateTime={time.toISOString()}> { time.toLocaleString() } </time>
+                            </span>
+                        </li>
+                    </ul>
                 </Popup>
             </Marker>
         </div>
