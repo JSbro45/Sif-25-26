615c304525dbd8a7d05abd0a9aabc3a8628b0b74
katdoormat <imsooweirdhaha@gmail.com>
2026-01-11 17:41:01 +0100

    eventview fix 2

--- next-frame/src/app/map/components/MarkerWindow.tsx
+++ next-frame/src/app/map/components/MarkerWindow.tsx
@@ -1,27 +1,31 @@
+'use client';
+
 import { Icon } from "leaflet";
 import { Marker, Popup } from "react-leaflet";
+import type { LatLngExpression } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import '../../styles/map.css'
 
-export default function MarkerWindow({pos,evt, time}: {pos: [number,number], evt: string, time: Date}) {
+export default function MarkerWindow({ pos, evt, time, onClick }: { pos: LatLngExpression, evt: any, time?: string, onClick?: () => void }) {
     const icon = new Icon({
         iconUrl: '/map-icon.svg',
         iconSize: [80, 80],
     });
 
     return (
-            <Marker position={pos} icon={icon}>
-                <Popup>
-                    <ul className="event-thumbnail">
-                        <li>
-                            <span className="event"> <b className="n"> event: </b> { evt } </span>
-                        </li>
-                        <li>
-                            <span className="time"> <b className="n"> time: </b>
-                                <time dateTime={time.toISOString()}> { time.toLocaleString() } </time>
-                            </span>
-                        </li>
-                    </ul>
-                </Popup>
-            </Marker>
+        <Marker position={pos} icon={icon} eventHandlers={{ click: () => onClick && onClick() }}>
+            <Popup>
+                <ul className="event-thumbnail">
+                    <li>
+                        <span className="event"> <b className="n"> event: </b> { evt } </span>
+                    </li>
+                    <li>
+                        <span className="time"> <b className="n"> time: </b>
+                            <time dateTime={time.toISOString()}> { time.toLocaleString() } </time>
+                        </span>
+                    </li>
+                </ul>
+            </Popup>
+        </Marker>
     )
 }
