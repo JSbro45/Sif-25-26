9abdc567e1a0d3e8f7a12d3276858a16a7f95830
JSbro45 <j.samuel.brou@gmail.com>
2026-01-22 21:13:33 +0100

    markerfix

--- next-frame/src/app/map/components/MarkerWindow.tsx
+++ next-frame/src/app/map/components/MarkerWindow.tsx
@@ -7,9 +7,8 @@ import { useState, useEffect, useRef } from "react";
 import 'leaflet/dist/leaflet.css'
 import '../../styles/map.css'
 
-export default function MarkerWindow({ pos, evt, time, dbClick }: { pos: LatLngExpression, evt: any, time: Date, dbClick?: () => void }) {
+export default function MarkerWindow({ pos, evt, time, clicked: clicked }: { pos: LatLngExpression, evt: any, time: Date, clicked?: () => void }) {
     const [isActive, setIsActive] = useState(false);
-    const markerRef = useRef<any>(null);
 
     const defaultIcon = new Icon({
         iconUrl: '/map-icon.svg',
@@ -21,37 +20,12 @@ export default function MarkerWindow({ pos, evt, time, dbClick }: { pos: LatLngE
         iconSize: [80, 85],
     });
 
-    const handleMarkerClick = (e: any) => {
-        e.originalEvent.stopPropagation();
-        setIsActive(!isActive);
-        dbClick && dbClick();
-    };
 
-    useEffect(() => {
-        const handleMapClick = (e: any) => {
-            // Only deactivate if clicking outside this marker
-            if (markerRef.current && !markerRef.current._container?.contains(e.originalEvent.target)) {
-                setIsActive(false);
-            }
-        };
-
-        if (markerRef.current) {
-            const map = markerRef.current._map;
-            if (map) {
-                map.on('click', handleMapClick);
-                return () => {
-                    map.off('click', handleMapClick);
-                };
-            }
-        }
-    }, []);
 
     return (
         <Marker 
-            ref={markerRef}
             position={pos} 
             icon={isActive ? activeIcon : defaultIcon} 
-            eventHandlers={{ click: handleMarkerClick }}
         >
         </Marker>
     )
