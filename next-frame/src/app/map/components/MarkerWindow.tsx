719fead793ac07b4ab880cd9a46b2350bce6c97e
katdoormat <imsooweirdhaha@gmail.com>
2026-02-01 13:43:33 +0100

    aktivni jen jeden marker na mape vyreseno

--- next-frame/src/app/map/components/MarkerWindow.tsx
+++ next-frame/src/app/map/components/MarkerWindow.tsx
@@ -3,23 +3,26 @@
 import { Icon } from "leaflet";
 import { Marker } from "react-leaflet";
 import type { LatLngExpression } from 'leaflet'
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import 'leaflet/dist/leaflet.css'
 import '../../styles/map.css'
 
 export default function MarkerWindow({ pos, evt, time, clicker, isActiveMarker, onActiveChange }: { pos: LatLngExpression, evt: any, time: Date, clicker?: () => void, isActiveMarker?: boolean, onActiveChange?: (active: boolean) => void }) {
-    const [isActive, setIsActive] = useState(false);
     const markerRef = useRef<any>(null);
 
     const icons = {
         default: new Icon({ iconUrl: '/map-icon.svg', iconSize: [60, 60] }),
         active: new Icon({ iconUrl: '/map-icon-active.png', iconSize: [80, 85] })
     };
-
+    
+    // Swichování mezi jednotlivými markery 
     useEffect(() => {
-        setIsActive(isActiveMarker || false);
+        if (markerRef.current) {
+            markerRef.current.setIcon(isActiveMarker ? icons.active : icons.default);
+        }
     }, [isActiveMarker]);
 
+    // Map tracking pro zavření markeru kliknutím mimo něj
     useEffect(() => {
         const map = markerRef.current?._map;
         if (!map) return;
@@ -38,10 +41,10 @@ export default function MarkerWindow({ pos, evt, time, clicker, isActiveMarker, 
         <Marker 
             ref={markerRef}
             position={pos} 
-            icon={isActive ? icons.active : icons.default}
+            icon={isActiveMarker ? icons.active : icons.default}
             eventHandlers={{ click: (e) => {
                 e.originalEvent.stopPropagation();
-                onActiveChange?.(!isActive);
+                onActiveChange?.(!isActiveMarker);
                 clicker?.();
             }}}
         />
