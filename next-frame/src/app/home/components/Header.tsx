c5a3eff007c2769d357d3d97fd6f871236ebb73e
katdoormat <imsooweirdhaha@gmail.com>
2026-02-23 08:57:49 +0100

    rychle nez to pushnes ty

--- next-frame/src/app/home/components/Header.tsx
+++ next-frame/src/app/home/components/Header.tsx
@@ -17,15 +17,15 @@ export default function Header() {
         <header className="header">
             <div id="logo_header">
             <img src="../public/icons/next/logo.svg"  alt="LocalFest logo" className="logo"/> {/*style="height:40px;width:40px;object-fit:contain;"*/}
-            <Link href="/home"> <h1 className="title">LocalFest.cz</h1> </Link>
+            <Link href="/home" id="title"> <h1>LocalFest.cz</h1> </Link>
             </div>
             <SearchBar />
             <nav className="nav">
                 <Link href="/events" className="links_header"> Eventy </Link>
                 <Link href="/home/about" className="links_header"> O Nás </Link>
                 <Link href="/home/kontakt" className="links_header"> Kontakty </Link>
-                <Link href="/auth/login" className="login_login"> Přihlásit se </Link>
-                <Link href="/auth/signup" className="login_singup"> Zaregistrovat se </Link>
+                <Link href="/auth/login" id="login"> Přihlásit se </Link>
+                <Link href="/auth/signup" id="signup"> Zaregistrovat se </Link>
             </nav>
         </header>
     )
