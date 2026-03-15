2809a7dbaaa711ea0649da7d56ff9f2cd9038fbe
katdoormat <imsooweirdhaha@gmail.com>
2026-03-15 16:50:31 +0100

    homepage css a doufam ze funkcni proklik na map page

--- next-frame/src/app/home/page.tsx
+++ next-frame/src/app/home/page.tsx
@@ -1,43 +1,49 @@
+'use client';
+
 //import { useState } from 'react'
 import Header from "./components/Header";
 import MapModule from '../map/components/MapModule';
 import '../styles/home.css'
 import PageWrapper from "./components/PageWrapper";
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
+    const router = useRouter();
     return (
         <PageWrapper> 
             <div className="home-container">
                         <h1 className="subtitle">LocalFest - pro kapely i pro hudební nadšence</h1>
                         <section className="page-section">
-                            <p className="text1">
-                            Máte volný víkend a rádi byste zašli poznávat nové hudební skupiny a žánry? Těšíte se na koncert svého oblíbeného interpreta, ale nevíte žádné bližší informace? 
-                            Nebo jste snad nadějná talentovaná kapela, která hledá příležitost zazářit a předávat svou hudbu svým fanouškům?
-                            <p style={{ fontSize: '22px', fontWeight: 'bold' }}>Tak to jste tady správně!</p>
-                            <ul>
-                                <li>LocalFest je webová aplikace, která vám pomůže najít koncerty a hudební akce v celé ČR rychle a přehledně, pomocí mapy. </li>
-                                <li>Na mapě jsou označené jednotlivé body představující místa konání koncertů a akcí, kdy po rozkliknutí se vám oběví bližší informace. </li>
-                            </ul>
-                            </p>
+                            <div className="text1">
+                                <p> 
+                                Máte volný víkend a rádi byste zašli poznávat nové hudební skupiny a žánry? Těšíte se na koncert svého oblíbeného interpreta, ale nevíte žádné bližší informace? 
+                                Nebo jste snad nadějná talentovaná kapela, která hledá příležitost zazářit a předávat svou hudbu svým fanouškům?
+                                </p>
+                                <p style={{ fontSize: '22px', fontWeight: 'bold' }}>Tak to jste tady správně!</p>
+                                <ul>
+                                    <li>LocalFest je webová aplikace, která vám pomůže najít koncerty a hudební akce v celé ČR rychle a přehledně, pomocí mapy. </li>
+                                    <li>Na mapě jsou označené jednotlivé body představující místa konání koncertů a akcí, kdy po rozkliknutí se vám objeví bližší informace. </li>
+                                </ul>
+                            </div>
                         </section>
                 <div className="map-text">
                         <section className="page-section">
-                            <p className="text2">
+                            <div className="text2">
                             <p style={{fontSize: 20 }}> Klikněte na mapu níže a začněte objevovat hudební akce <br></br> ve vašem okolí!
                                 <br></br>➜
                             </p>
                             <ul style={{padding: '10px 0px 0px 20px'}}>
-
-                                <li style={{paddingBottom: '10px'}}>Prostřednictvím ikonek v levém horním rohu můžete vyhledávat konkrétní název akce, interpreta nebo místa a určit si i časový rozsah, ve kterém chcete akce vyhledávat.</li>
+                                <li style={{paddingBottom: '10px'}}>Po kliknutí na mapu můžete prostřednictvím ikonek v levém horním rohu vyhledávat konkrétní název akce, interpreta nebo místa a určit si i časový rozsah, ve kterém chcete akce vyhledávat.</li>
                                 <li>Pokud chcete svůj koncert zařadit do naší databáze, můžete tak učinit prostřednictvím vytvoření účtu pro pořadatele. 
-                                Stačí kliknout na tlačítko "Zaregistrovat se" v pravém horním rohu.</li>
+                                Stačí kliknout na tlačítko "Zaregistrovat se" na domovské stránce.</li>
                             </ul>
-                            <br />Pro jakékoliv dotazy či připomínky nás neváhejte kontaktovat prostřednictvím <br />sekce "O nás". Rádi vám pomůžeme!
+                            <p>
+                                <br />Pro jakékoliv dotazy či připomínky nás neváhejte kontaktovat prostřednictvím <br />sekce "O nás". Rádi vám pomůžeme!
                             </p>
+                            </div>
                         </section>
                     <div className="map-contain-home">
-                        <MapModule map_type='embed' markers={[]} />    
-                        {/* zařiď ať je mapa napravo */}   
+                        <MapModule map_type='embed' markers={[]} onEmbedMapClick={() => router.push('/map')}/>    
                     </div>
                 </div>    
             </div>
--- next-frame/src/app/map/components/MapModule.tsx
+++ next-frame/src/app/map/components/MapModule.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import { MapContainer, TileLayer } from 'react-leaflet' 
+import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet' 
 import MarkerWindow from './MarkerWindow'
 import 'leaflet/dist/leaflet.css'
 import { MarkerProps } from '../../../lib/map-types'
 import { getPins } from '@/src/lib/data-fetch';
 import { LatLngTuple } from 'leaflet';
 
-
-export default function MapModule({ map_type, markers, onMarkerClick, activeMarkerEvent, onMarkerActiveChange } : {
+export default function MapModule({ map_type, markers, onMarkerClick, activeMarkerEvent, onMarkerActiveChange, onEmbedMapClick } : {
     map_type: 'page' | 'embed',
     markers : MarkerProps[], 
     onMarkerClick?: (payload: MarkerProps) => void, 
     activeMarkerEvent?: MarkerProps | null, 
-    onMarkerActiveChange?: (event: MarkerProps | null) => void 
+    onMarkerActiveChange?: (event: MarkerProps | null) => void,
+    onEmbedMapClick?: () => void
 }) {
   const API_KEY = 'lijiPKo4X8TaQxEXRTHg_8ySYzbGEwoVTL6YILGdk78'
 
+  function MapClickHandler({ onClick }: { onClick?: () => void }) {
+    useMapEvents({
+      click: onClick,
+    });
+    return null;
+  }
 
   return (
     <div className={ map_type + '-map'}>
@@ -25,6 +31,7 @@ export default function MapModule({ map_type, markers, onMarkerClick, activeMark
           attribution='<a href="https://api.mapy.com/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>'
           url={`https://api.mapy.com/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${API_KEY}`}
         />
+        <MapClickHandler onClick={onEmbedMapClick} />
         {
           
           markers?.map((mark, key) => 
